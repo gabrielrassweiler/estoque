0e13e09463345f20147f7756d734032a2c2d6685
gabrielrassweiler <gabriel_rassweiler@hotmail.com>
2022-06-08 10:40:32 -0300

    feat(dark-mode): add toggle para dark mode

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -15,7 +15,15 @@
           Quasar App
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <div>
+          <q-toggle
+            v-model="dark"
+            unchecked-icon="brightness_5"
+            checked-icon="nightlight_round"
+            color="dark"
+            @click="$q.dark.toggle()"
+          />
+        </div>
       </q-toolbar>
     </q-header>
 
@@ -55,42 +63,6 @@ const linksList = [
     caption: 'quasar.dev',
     icon: 'school',
     link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
   }
 ]
 
@@ -102,9 +74,11 @@ export default defineComponent({
   },
 
   setup () {
+    const dark = ref(false)
     const leftDrawerOpen = ref(false)
 
     return {
+      dark,
       essentialLinks: linksList,
       leftDrawerOpen,
       toggleLeftDrawer () {
