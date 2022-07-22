0b75a0531bf63990ff88dae5a39961b4345e737b
gabrielrassweiler <gabriel_rassweiler@hotmail.com>
2022-07-22 14:18:11 -0300

    refactor(login): personalização da tela de login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,24 +1,32 @@
 <template>
-  <q-page padding>
-    <div class="row justify-center" style="position: relative; top: 15vh;">
-      <q-card class="col-md-4 col-sm-7 col-xs-10" bordered>
+  <q-page>
+    <div class="row justify-center q-gutter-x-lg">
+      <img class="imagem-fundo" src="../assets/wave.svg" alt="imagem" />
+
+      <div class="col-md-4 col-sm-7 col-xs-10" style="position: relative; top: 10vh; text-align: center">
+        <p class="text-h6" style="color: #e6e6e6;">Gerencie seu estoque e loja</p>
+        <p class="text-h5 text-weight-bold" style="color: #2c9c6a">Faça login ou cadastre-se agora mesmo!</p>
+
+        <img class="imagem-estoque" src="../assets/animacao.svg" alt="imagem-estoque" />
+      </div>
+
+      <q-card class="col-md-4 col-sm-7 col-xs-10 card-main" bordered>
         <q-card-section class="q-mt-lg q-mb-lg">
           <q-form @submit.prevent="handleLogin">
             <p class="col-12 text-h5 text-center">Login</p>
 
-            <div class="q-gutter-y-lg q-mt-lg">
-              <q-input
-                label="Email"
-                v-model="form.email"
-                type="email"
-              />
-              <q-input
-                label="Senha"
-                v-model="form.password"
-                type="password"
-              />
-
-              <div class="full-width q-pt-md">
+            <div>
+              <div class="q-gutter-y-lg q-mt-lg q-mb-lg">
+                <q-input
+                  label="Email"
+                  v-model="form.email"
+                  type="email"
+                />
+                <q-input
+                  label="Senha"
+                  v-model="form.password"
+                  type="password"
+                />
                 <q-btn
                   label="Login"
                   color="primary"
@@ -28,10 +36,11 @@
                   type="submit"
                 />
               </div>
+
               <q-btn
                 label="Registrar-se"
                 color="primary"
-                class="full-width q-gutter-y-sm"
+                class="full-width q-gutter-y-sm q-mt-lg"
                 flat
                 rounded
                 to="/registrar"
@@ -99,3 +108,26 @@ export default defineComponent({
   }
 })
 </script>
+
+<style scoped>
+  .imagem-fundo {
+    position: fixed;
+    bottom: 0;
+    margin: auto;
+    width: 105%;
+    height: 50%;
+    object-fit: cover;
+  }
+
+  .imagem-estoque {
+    position: relative;
+    width: 75%;
+  }
+
+  .card-main {
+    top: 11vh;
+    height: 500px;
+    box-shadow: 0 1px 15px #383838;
+    margin-bottom: 160px;
+  }
+</style>
