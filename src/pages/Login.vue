15dac1ef60f3ca27beba6b53b3117d41f43857bc
gabrielrassweiler <gabriel_rassweiler@hotmail.com>
2022-07-22 10:06:19 -0300

    feat(login): add card envolvendo form

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,48 +1,54 @@
 <template>
   <q-page padding>
-    <q-form class="row justify-center" style="top: 20vh;" @submit.prevent="handleLogin">
-      <p class="col-12 text-h5 text-center">Login</p>
+    <div class="row justify-center" style="position: relative; top: 15vh;">
+      <q-card class="col-md-4 col-sm-7 col-xs-10" bordered>
+        <q-card-section class="q-mt-lg q-mb-lg">
+          <q-form @submit.prevent="handleLogin">
+            <p class="col-12 text-h5 text-center">Login</p>
 
-      <div class="col-md-4 col-sm-6 col-xs-10 q-gutter-y-lg">
-        <q-input
-          label="Email"
-          v-model="form.email"
-          type="email"
-        />
-        <q-input
-          label="Senha"
-          v-model="form.password"
-          type="password"
-        />
+            <div class="q-gutter-y-lg q-mt-lg">
+              <q-input
+                label="Email"
+                v-model="form.email"
+                type="email"
+              />
+              <q-input
+                label="Senha"
+                v-model="form.password"
+                type="password"
+              />
 
-        <div class="full-width q-pt-md">
-          <q-btn
-            label="Login"
-            color="primary"
-            class="full-width"
-            outline
-            rounded
-            type="submit"
-          />
-        </div>
-        <q-btn
-          label="Registrar-se"
-          color="primary"
-          class="full-width q-gutter-y-sm"
-          flat
-          rounded
-          to="/registrar"
-        />
-        <q-btn
-          label="Esqueceu a senha ?"
-          color="primary"
-          class="full-width"
-          flat
-          rounded
-          to="/esqueceu-senha"
-        />
-      </div>
-    </q-form>
+              <div class="full-width q-pt-md">
+                <q-btn
+                  label="Login"
+                  color="primary"
+                  class="full-width"
+                  outline
+                  rounded
+                  type="submit"
+                />
+              </div>
+              <q-btn
+                label="Registrar-se"
+                color="primary"
+                class="full-width q-gutter-y-sm"
+                flat
+                rounded
+                to="/registrar"
+              />
+              <q-btn
+                label="Esqueceu a senha ?"
+                color="primary"
+                class="full-width"
+                flat
+                rounded
+                to="/esqueceu-senha"
+              />
+            </div>
+          </q-form>
+        </q-card-section>
+      </q-card>
+    </div>
   </q-page>
 </template>
 
